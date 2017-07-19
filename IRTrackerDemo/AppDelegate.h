75449c559aafa688ab5b0d6c0a02b106a8c79fb0
文楷 <wanlei.gwl@alibaba-inc.com>
2017-07-19 17:18:51 +0800

    Initial Commit

---
+++ IRTrackerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IRTrackerDemo
+//
+//  Created by Wanlei on 2017/7/19.
+//  Copyright © 2017年 IR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IRTrackerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IRTrackerDemo
+//
+//  Created by Wanlei on 2017/7/19.
+//  Copyright © 2017年 IR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
